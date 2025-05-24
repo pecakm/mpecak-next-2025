7f0fde94e98de3797b5ad58e383651dec4299b8b
Mikołaj Pęcak <mikolajpecak@gmail.com>
2025-05-24 10:09:13 +0200

    Move i18n navigation to deprecated files

--- components/languageButton/languageButton.tsx
+++ components/languageButton/languageButton.tsx
@@ -1,9 +1,7 @@
 'use client';
 
-import { useLocale } from 'next-intl';
 import { useParams, useRouter, usePathname } from 'next/navigation';
 
-// import { useRouter, usePathname } from '@/i18n';
 import { CountryCode } from '@/enums';
 
 import { HeaderButton } from '../common.styled';
@@ -11,15 +9,12 @@ import { HeaderButton } from '../common.styled';
 export default function LanguageButton() {
   const router = useRouter();
   const pathname = usePathname();
-  const currentLocale = useLocale();
   const { locale } = useParams();
-  console.log(currentLocale, locale);
   const nextLocale = locale === CountryCode.PL ? CountryCode.EN : CountryCode.PL;
   const nextPathname = pathname.replace(`/${locale}`, `/${nextLocale}`);
 
   const handleClick = () => {
     router.replace(nextPathname);
-    // router.replace(pathname, { locale: nextLocale });
   };
 
   return (
--- i18n/index.ts
+++ i18n/index.ts
@@ -1,3 +1,3 @@
 export * from './routing';
-export * from './navigation';
+export * from './navigation.deprecated';
 export * from './useLocalePath';
