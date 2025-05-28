1e51af940024f31689db86c51d19b11eac2dfae8
Mikołaj Pęcak <mikolajpecak@gmail.com>
2025-05-28 19:30:15 +0200

    Update LinkedIn link

--- constants/links.constants.ts
+++ constants/links.constants.ts
@@ -1,5 +1,5 @@
 export const Links = {
-  LinkedIn: 'https://pl.linkedin.com/in/mikołaj-pęcak-1649a612b',
+  LinkedIn: 'https://www.linkedin.com/in/mikolaj-pecak',
   GitHub: 'https://github.com/pecakm',
   Email: 'mailto:mikolajpecak@gmail.com',
   ITPulse: 'https://itpulse.pl',
